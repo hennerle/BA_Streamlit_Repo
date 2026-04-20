c50f1c8d472923ee561d6622426059fd21b02fa2
Hendrik Martin <hendrik.martin@iee.fraunhofer.de>
2026-04-20 10:31:59 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,13 @@ import streamlit as st
 import json
 import pandas as pd
 
-st.header("This is the flexmatrix displayed as a plain Matrix", divider="gray")
+st.header("This is the flexmatrix displayed as a plain table", divider="gray")
 st.markdown("The flexmatrix shows all flexibility options given the schedule provided by the depots.")
 
 data = json.load(open("flex_message.json"))
 
 df = pd.DataFrame(data)
 st.dataframe(df)
+
+
+print(data.messageId)
